8cf3b66d0a9581294d6ae6cab70c9476f5d49acb
AKHLAK ANSARI <87934721+AkhlakAnsari@users.noreply.github.com>
2021-09-06 17:03:56 +0530

    Add files via upload

---
+++ multiplication table using forloop.py
@@ -0,0 +1,6 @@
+#creat multiplication table using forloop
+number=int(input('ENTER THE VALUE OF TABLE:'))
+for count in range(1,11):
+ result=number*count
+ print(number ,"*",count,"=",result)
+#thanks akhlakansari94@gmail.com
